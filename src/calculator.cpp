669324b70ffe222183cca8b3a0516adcfc09707e
abdulla-ashurov <abdulla.ashurov@dsr-corporation.com>
2022-10-29 23:59:16 +0500

    Refactor code

--- src/calculator.cpp
+++ src/calculator.cpp
@@ -11,16 +11,6 @@ namespace calc
         return calculate_impl(prefix);
     }
 
-    Token::Token(const std::string &value)
-        : value{value}
-    {
-        type = define_type(value);
-        possible_left_value = define_possible_left_value(type);
-    }
-
-    Token::Token(const char value)
-        : Token(std::string(1, value)) {}
-
     tokens parse(const std::string &expr)
     {
         std::string operand;
@@ -44,4 +34,14 @@ namespace calc
 
         return infix;
     }
+
+    Token::Token(const std::string &value)
+        : value{value}
+    {
+        type = define_type(value);
+        possible_left_value = define_possible_left_value(type);
+    }
+
+    Token::Token(const char value)
+        : Token(std::string(1, value)) {}
 }
